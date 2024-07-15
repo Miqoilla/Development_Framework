f2b3f2f46ff573e3eec8c25cb5fa96b263a423a5
Miqoilla <Miqoilla@gmail.com>
2024-07-15 20:17:24 +0700

    update

--- resources/views/rak_buku/form.blade.php
+++ resources/views/rak_buku/form.blade.php
@@ -1,37 +1,39 @@
 @extends('layouts.app')
+
 @section('title', 'Daftar Rak Buku')
+
 @section('content')
     <script>
         $(function() {
-        $("#btn-save").click(function(e) {
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
-                }
+            $("#btn-save").click(function(e) {
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                e.preventDefault();
+                var formData = {
+                    nama: $('#nama').val(),
+                    lokasi: $('#lokasi').val(),
+                    keterangan: $('#keterangan').val()
+                };
+                var state = $('#btn-save').val();
+                var type = "POST";
+                var id = $('#id').val();
+                var ajaxurl = '{{ $action }}';
+                $.ajax({
+                    type: type,
+                    url: ajaxurl,
+                    data: formData,
+                    dataType: 'json',
+                    success: function(data) {
+                        var todo = 'Pengiriman Data berhasil';
+                    },
+                    error: function(data) {
+                        console.log(data);
+                    }
+                });
             });
-            e.preventDefault();
-            var formData = {
-                nama: $('#nama').val(),
-                lokasi: $('#lokasi').val(),
-                keterangan: $('#keterangan').val()
-            };
-            var state = $('#btn-save').val();
-            var type = "POST";
-            var id = $('#id').val();
-            var ajaxurl = '{{ $action }}';
-            $.ajax({
-                type: type,
-                url: ajaxurl,
-                data: formData,
-                dataType: 'json',
-                success: function(data) {
-                    var todo = 'Pengiriman Data berhasil'
-                },
-                error: function(data) {
-                    console.log(data);
-                }
-            });
-        });
         });
     </script>
     <script>
@@ -47,23 +49,31 @@
             @method('PUT')
         @endif
         <input type="hidden" name="id" value="{{ $rak->id }}" />
+        
         <input type="text" class="mail_text" id="nama" name="nama" placeholder="Nama Rak" value="{{ $rak->nama }}" /><br>
         @error('nama')
             <b>{{ $message }}</b>
         @enderror
         <br>
-        <input type="text" class="mail_text" id="lokasi"  name="lokasi" placeholder="Lokasi" value="{{ $rak->lokasi }}" /><br>
+        
+        <input type="text" class="mail_text" id="lokasi" name="lokasi" placeholder="Lokasi" value="{{ $rak->lokasi }}" /><br>
         @error('lokasi')
             <b>{{ $message }}</b>
         @enderror
         <br>
-        <input type="text" class="mail_text" id="keterangan"  name="keterangan" placeholder="keterangan"
-            value="{{ $rak->keterangan }}" /><br>
-        <p><input type="text" id="datepicker" name="tanggal" placeholder="tanggal"></p>
-        {{-- <input type="submit" value="{{ $store }}" /> --}}
-        <button class="save_bt" type="submit" id="btn_save">
-            Simpan
-        </button>
+        
+        <div class="form-group">
+            <input type="text" class="mail_text" id="keterangan" name="keterangan" placeholder="Keterangan" value="{{ $rak->keterangan }}" />
+        </div>
+        <br>
+
+        <div class="form-group">
+            <input type="text" class="mail_text" id="datepicker" name="tanggal" placeholder="Tanggal">
+        </div>
+        <br>
+
+        <button class="btn btn-primary save_bt" type="submit" id="btn_save" style="margin-top: 20px;">Simpan</button>
+        
         <div class="send_bt">
             <a href="{{ url('/rak_buku') }}">Kembali</a>
         </div>
